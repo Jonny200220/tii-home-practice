2b1413bea606e1f057df3e8266fb320fd386ad5f
JonnyRmz <jonnyrmzhdz@gmail.com>
2025-12-26 01:22:23 -0600

    feat: agregando tabs de navegacion al proyecto

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,47 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Feather from "@expo/vector-icons/Feather";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <React.Fragment>
+      <StatusBar style="auto" />
+      <Tabs screenOptions={{ tabBarActiveTintColor: "teal" }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            tabBarIcon: () => <Feather name="home" size={24} color="black" />,
+          }}
+        />
+        <Tabs.Screen
+          name="Historial"
+          options={{
+            tabBarIcon: () => (
+              <AntDesign name="history" size={24} color="black" />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="Ajustes"
+          options={{
+            tabBarIcon: () => (
+              <Feather name="settings" size={24} color="black" />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="Perfil"
+          options={{
+            tabBarIcon: () => (
+              <FontAwesome5 name="user" size={24} color="black" />
+            ),
+          }}
+        />
+      </Tabs>
+    </React.Fragment>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +9,12 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Buenos Dias</Text>
+      <Text>Que hay que Arreglar hoy?</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  
+})
